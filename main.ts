80972d6388a29729046afc691778bf51a94e166b
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-04-02 13:29:19 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,15 +5,37 @@
  * This program can drive a car and avoid collision.
 */
 
+// variable
+let numDist: number = 0
+
 // setup
 basic.showIcon(IconNames.Happy)
 robotbit.StpCarMove(0, 48)
 
-// loop
+// loop forever
 while (true) {
-  if (input.buttonIsPressed(button.A) == true) {
+  // find distance
+  numDist = sonar.ping(
+    DigitalPin.P1, // trigger
+    DigitalPin.P2, // echo
+    PingUnit.Centimeters
+  )
+
+  // moving car
+  if (input.buttonIsPressed(Button.A) == true) {
     robotbit.StpCarMove(20, 48)
-    
-
-
+    basic.showString("moving")
+    basic.showIcon(IconNames.Yes)
+  
+  // avoid collision
+    if (numDist < 10) {
+      robotbit.StpCarMove(0, 48)
+      basic.showIcon(IconNames.Triangle)
+      pause(500)
+      robotbit.StpCarMove(-10, 48)
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0) // turns 1 motor 180 degrees
+      pause(500)
+      robotbit.StpCarMove(20, 48)
+    }
   }
+}
